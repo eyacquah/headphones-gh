f3d27ea6e98777a43d1f9150715b0047e1d01873
eyacquah <eyacquah0@gmail.com>
2021-04-09 14:44:47 +0000

    Minor changes to frontend

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,19 +1,17 @@
-const Product = require("../models/productModel");
-const catchAsync = require("../utils/catchAsync");
+// const Product = require("../models/productModel");
+// const catchAsync = require("../utils/catchAsync");
 
 exports.renderContactUs = (req, res, next) => {
   res.status(200).render("contact", { title: "Contact Us" });
 };
 
-exports.renderAboutUs = async (req, res, next) => {
+exports.renderAboutUs = (req, res, next) => {
   res.status(200).render("about", { title: "About Us" });
 };
 
-exports.getIndex = catchAsync(async (req, res, next) => {
-  const products = await Product.find();
-
-  res.status(200).render("index", { products, title: "Home" });
-});
+exports.getIndex = (req, res, next) => {
+  res.status(200).render("index", { title: "Home" });
+};
 
 exports.getCheckout = (req, res) => {
   res.status(200).render("checkout", { title: "Checkout" });
